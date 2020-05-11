436faebe623cde7cc453d87733b5c9019d767d3d
Gustavo Brito <gustavojsb@hotmail.com>
2020-05-10 23:54:49 -0300

    text analysis

--- tsentiment.py
+++ tsentiment.py
@@ -1,3 +1,5 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 class TSentiment:
     def __init__(self, text,language):
         self._text = text
@@ -12,12 +14,23 @@ class TSentiment:
         return self._language
 
     def analyze_feeling(self):
-        if self.language == 'pt':
-            #analyse text in PT BR
-            print('')
-        elif self.language == 'en':
-            # analyse text in EN
-            print()
+        sid = SentimentIntensityAnalyzer()
 
-        return "good|bad|neutral"  # analyse text
+        print(sid.lexicon_file)
+        ss = sid.polarity_scores(self._text)
+        for k in sorted(ss):
+            print('{0}: {1}, '.format(k, ss[k]), end='')
+        
+        print()
+        print()
+
+#
+#        if self.language == 'pt':
+#            #analyse text in PT BR
+#            print('')
+#        elif self.language == 'en':
+#            # analyse text in EN
+#            print()
+#
+#        return "good|bad|neutral"  # analyse text
 
