049813ab02f2dcbc790b4413874bc1fbc6385c5b
Gustavo Brito <gustavojsb@hotmail.com>
2020-11-03 00:57:53 -0300

    baseline implementation

--- train_intelligence.py
+++ train_intelligence.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.dummy import DummyRegressor
 import pandas as pd
 import pickle
 import text_treatment
@@ -23,7 +24,7 @@ def train_intelligence(dataframe, text_column, classification_column):
     bag_of_words = vectorizer.fit_transform(dataframe[text_column])
 
     train, test, class_train, class_test = train_test_split(bag_of_words, dataframe[classification_column],
-                                                            random_state=42, test_size=0.25)
+                                                            random_state=42, test_size=0.15)
 
     logistic_regression = LogisticRegression()
     logistic_regression.fit(train, class_train)
@@ -41,9 +42,15 @@ def train_intelligence(dataframe, text_column, classification_column):
     filename = 'anton_vectorizer.sav'
     pickle.dump(vectorizer, open(filename, 'wb'))
 
-    return round(logistic_regression.score(test, class_test) * 100, 2)
+    ###### baseline ######
+    baseline = DummyRegressor(strategy="mean")
+    baseline.fit(train, class_train)
+    print('Baseline Accuracy: ')
+    print(round(baseline.predict(class_test)[0] * 100, 2))
+    ###### baseline ######
 
+    print('Algorithm Accuracy:')
+    print(round(logistic_regression.score(test, class_test) * 100, 2))
+    return
 
-print(train_intelligence(mass_data_reviews, 'treated_text', 'classification'))
-
-
+train_intelligence(mass_data_reviews, 'treated_text', 'classification')
